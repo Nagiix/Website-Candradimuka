8c68f52bb13b09ea27253aca3acace0df5fc05dc
Nagiix <dimas.nauli@gmail.com>
2024-12-25 22:23:30 +0800

    Header, Functionality Header, Landing Page, Transition page to section 1, Transition page functionality, Section 1, Section 1 Functionality, WIP section 2

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Website-Candradimuka
 For: Website Project Candradimuka - UAS
 Framework: NextJS
-Template/Type: 1 Page
-Language: ???
+Language: CSS/HTML/JS
--- project_candradimuka/src/app/page.tsx
+++ project_candradimuka/src/app/page.tsx
@@ -1,101 +1,207 @@
+'use client'
 import Image from "next/image";
+import React, { useRef, useEffect, useState } from "react";
+// import { items } from "./items.json";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Carousel } from "react-bootstrap";
 
 export default function Home() {
+  // OnClick scroll to section
+  const [isInView, setIsInView] = useState(false);
+  const sectionOneRef = useRef<HTMLDivElement>(null);  
+  const sectionZeroRef = useRef<HTMLDivElement>(null); 
+  const textRef_Characters = useRef<HTMLDivElement>(null); 
+  
+  // // Carousel
+  // const { bootstrap } = items;
+  // const [index, setIndex] = useState(0);
+  // const handleSelect = (selectedIndex , e) => {
+  //   setIndex(selectedIndex);
+  // };
+
+  // Scroll to Section one
+  const scrollToSectionOne = () => {
+    sectionOneRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  };
+
+  //check view state of reader
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Check if the section is in view
+        if (entry.isIntersecting) {
+          setIsInView(true); // Apply bold when the section is in view
+        } else {
+          setIsInView(false); // Remove bold when the section is out of view
+        }
+      },
+      {
+        threshold: 0.5, // Trigger when 50% of the section is in view
+      }
+    );
+    if (sectionOneRef.current) {
+      observer.observe(sectionOneRef.current);
+    }
+    return () => {
+      if (sectionOneRef.current) {
+        observer.unobserve(sectionOneRef.current);
+      }
+    };
+  }, []);
+
+  // main
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        {/* Background - Landing*/}
+        <div className="Background_image" ref={sectionZeroRef}>
+          {/* <Image
+            src="/res_img/bg_main_resized.png"
+            alt="test"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          /> */}
+        </div>
+          
+        {/* Navbar */}
+        <div className="navbar">
+          <div className="navbarbox"></div>
+          <div className="navbar-content">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="Language"
+              src="/res_img/Icons/dictionarylanguagebook.png"
+              alt="translate"
+              width={48}
+              height={48}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="title">
+              <h1>Project Candradimuka</h1>
+            </div>
+            <ul className="navbar_links">
+              <li>
+                <div className="Character_text" ref={textRef_Characters}
+                style={{
+                  WebkitTextStroke: isInView? "2px #000000" : "0px #000000"
+                }}
+                >
+                  <a onClick={scrollToSectionOne}>Characters</a>
+                </div>
+              </li>
+              <li><a href="">Mechanics</a></li>
+              <li><a href="">Story</a></li>
+              <li><a href="">World</a></li>
+            </ul>
+            <ul className="navbar_socials">
+              {/* Twitter */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="black" className="bibi-twitter-x" viewBox="0 0 16 16">
+              <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+            </svg>
+              {/* insta */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="black" className="bibi-instagram" viewBox="0 0 16 16">
+              <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+            </svg>
+            {/* Youtube */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="black" className="bibi-youtube" viewBox="0 0 16 16">
+              <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
+            </svg>
+            </ul>
+          </div>
+        </div>
+
+        {/* Transition */}
+        <div className="transition">
+          <div className="tr-1">
+            <svg width="100vw" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFB353" fillOpacity="1" d="M0,128L21.8,117.3C43.6,107,87,85,131,69.3C174.5,53,218,43,262,48C305.5,53,349,75,393,96C436.4,117,480,139,524,160C567.3,181,611,203,655,186.7C698.2,171,742,117,785,117.3C829.1,117,873,171,916,170.7C960,171,1004,117,1047,85.3C1090.9,53,1135,43,1178,32C1221.8,21,1265,11,1309,42.7C1352.7,75,1396,149,1418,186.7L1440,224L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path></svg>
+          </div>
+          <div className="tr-2">
+            <svg width="100vw" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFBC04" fillOpacity="0.4" d="M0,128L21.8,117.3C43.6,107,87,85,131,69.3C174.5,53,218,43,262,48C305.5,53,349,75,393,96C436.4,117,480,139,524,160C567.3,181,611,203,655,186.7C698.2,171,742,117,785,117.3C829.1,117,873,171,916,170.7C960,171,1004,117,1047,85.3C1090.9,53,1135,43,1178,32C1221.8,21,1265,11,1309,42.7C1352.7,75,1396,149,1418,186.7L1440,224L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path></svg>
+          </div>
+          <div className="tr-3">
+            <svg width="100vw" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EC4043" fillOpacity="0.2" d="M0,128L21.8,117.3C43.6,107,87,85,131,69.3C174.5,53,218,43,262,48C305.5,53,349,75,393,96C436.4,117,480,139,524,160C567.3,181,611,203,655,186.7C698.2,171,742,117,785,117.3C829.1,117,873,171,916,170.7C960,171,1004,117,1047,85.3C1090.9,53,1135,43,1178,32C1221.8,21,1265,11,1309,42.7C1352.7,75,1396,149,1418,186.7L1440,224L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path></svg>
+          </div>
+          <div className="transition_text">
+            Continue?
+            <div className="transition_image">
+            <Image
+              className="arrowdowntr"
+              src="/res_img/Icons/arrow_down.png"
+              alt="Arrow_down"
+              width = {48}
+              height = {48}
+              onClick={scrollToSectionOne}
+            />  
+            </div>
+          </div>
+        </div>
+        {/* Section 1 */}
+        <div className="section1" >
+          <div className="s1_guideline">
+            <Image
+              className="s1top_guideline"
+              src="/res_img/Icons/outline_single-pg1.png"
+              alt="outline"
+              width = {1790}
+              height = {869}
+              quality={100}
+            />
+            <Image
+              className="s1bottom_guideline"
+              src="/res_img/Icons/outline_single-pg1.png"
+              alt="outline"
+              width = {1790}
+              height = {869}
+              quality={100}
+            />
+          </div>
+          <div className="s1_text">
+            <div className="s1_text_top">
+              A Guide to an old world filled with mysticism.
+            </div>
+            <div className="s1_text_bottom" ref={sectionOneRef}>
+              Served by yours truly, the Dalangs.
+            </div>
+            <div className="s1_text_blackbox">
+              Stand With Dharma, Walk With Karma, Stand Against The Adharma.
+            </div>
+          </div>
+          <Image
+            className="s1_arrowdown"
+            src="/res_img/Icons/arrow_down.png"
+            alt="Arrow_down"
+            width = {48}
+            height = {48}
+          />
+        </div>
+
+        {/* section 2
+        <div className="section2">
+          <div className="s2_contents">
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+              {bootstrap.map((item) => (
+                <Carousel.Item key={item.id} className="styles.itemP" interval={4000}>
+                  <Image src={item.imageUrl} alt="slides" width={100} height={100}/>
+                  <Carousel.Caption className="styles.caption">
+                    <h3>{item.title}</h3>
+                    <p>{item.body}</p>
+                    <button className="btn btn-danger">Visit Docs</button>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </div>
+        </div> */}
+
+        {/* Scrollable website */}
+        <div className="extra-content">
+          <p>This is additional content that makes the page scrollable.</p>
+          <p>More content here...</p>
+          <p>Even more content!</p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
